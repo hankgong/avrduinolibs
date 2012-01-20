59cc919bdc31feb5892dc88164bea7d73d93f325
Huazhi (Hank) GONG <hankgong@gmail.com>
2012-01-20 16:40:02 -0500

    add a sample file

---
+++ test/simpleprint.cpp
@@ -0,0 +1,13 @@
+#include <WProgram.h>
+#include <hserial.h>
+#include <avr/io.h>
+#include <stdio.h>
+#include <pinterrupt.h>
+#include <Wire.h>
+
+int main(void){
+	for(;;){
+		printf("test me\n");
+		delay(500);
+	}
+}
